28d730cdc741456574cf2c2a4b4692b0c85eb3c5
moyu <18810191901@163.com>
2020-11-07 21:29:18 -0800

    Templates.1.0

---
+++ Templates/include/defs.h
@@ -0,0 +1,38 @@
+#ifndef  _DEFS_H
+#define  _DEFS_H
+
+
+/*
+ *  Word notes:
+ *  original            Alias               meaning
+ *  Alias               ---                 别名
+ *  original            ---                 原始
+ *  Portable            PR                  可移植
+ *
+ *  Author:Moyu         date:2020-10-31
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+typedef  signed     int     INT_PR;
+typedef  unsigned   int     UNINT_PR;
+typedef  char       CHAR_PR;
+typedef  short      SHORT_PR;
+typedef  unsigned   short   UNSHORT_PR;
+typedef  long       LONG_PR;
+typedef  unsigned   long    UNLONG_PR;
+typedef  double     DOUBLE_PR;
+typedef  float      FLOAT_PR;
+
+#endif
---
+++ Templates/src/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "defs.h"
+int main()
+{
+    printf("Hello Project\n");
+    
+    INT_PR count;
+
+    for(int i = 0; i < 10; i++)
+    {
+        count++;
+    }
+
+
+    printf("------count = %d------\n",count);
+    printf(" Bye  Project\n");
+    return 0;
+}
+
+
+
